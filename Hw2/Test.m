23c8094de3f66adee3e74814da577d7874a3513e
Ron53267 <115971170+Ron53267@users.noreply.github.com>
2023-04-28 18:43:55 +0800

    timelist

--- Hw2/Test.m
+++ Hw2/Test.m
@@ -1,16 +1,14 @@
 clc;clear
 A = [0 1; 2 5; 10  12; 4 6; 3 8];
 B = sortrows(A, 1);
-C = reshape(B.', 1, numel(B));
-D = [];
-D(1) = C(1);
-G = 1;
-for check_n = 2:numel(C)
-    if C(check_n) <= D(G)
-        continue
-    elseif 
+C = B(1,:);
+for check_n = 2:size(B, 1)
+    if C(end, 2) < B(check_n, 1)
+        C(end+1, :) = B(check_n, :);
+    else
+        C(end, 2) = max(C(end, 2), B(check_n, 2));
     end
-    
-    G = G+1;
-    D(G) = C(check_n);
 end
+
+
+
